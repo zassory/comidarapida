d735e2771a781424fb462596adf66d364a4d599d
zassory <nicolas.programador@gmail.com>
2022-12-21 15:24:25 -0300

    obteniendo datos mi bd con next y getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
+import { PrismaClient } from '@prisma/client';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({categorias}) {
+  console.log(categorias);
   return (
     <h1>Next.js</h1>
   )
 }
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+  const categorias = await prisma.categoria.findMany();
+  console.log("En el servidor: ", categorias);
+  
+  return {
+    props: {
+      categorias,
+    }
+  }
+};
