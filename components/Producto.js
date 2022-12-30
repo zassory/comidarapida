8d8ea3217a4a525a1a06cf51744c9be0d4da9db4
zassory <nicolas.programador@gmail.com>
2022-12-30 19:51:18 -0300

    mostrando categoria actual en nuevo componente de producto, gracias al eager loading automaticamente trae los productos sin necesidad de cambiar el endpoint en el useeffect y sin necesidad de crear multiples archivos

---
+++ components/Producto.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { formatearDinero } from "../helpers";
+
+export const Producto = ({producto}) => {
+ 
+  const { nombre , imagen , precio } = producto;
+
+  return (
+    <div className="border p-3">
+        <Image 
+        src={`/assets/img/${imagen}.jpg`} 
+        alt={`Imagen Platillo ${nombre}`}
+        width={400}
+        height={500}
+        />
+        <div className="p-5">
+            <h3 className="text-2xl font-bold">{nombre}</h3>
+            <p className="mt-5 font-black text-4xl text-amber-500">
+                {formatearDinero(precio)}
+            </p>
+        </div>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,15 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 
 import { Layout } from "../layout/Layout";
+import { Producto } from "../components/Producto";
 import { useQuiosco } from '../hooks/useQuiosco';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
    
-  const { categoriaActual } = useQuiosco();  
+  const { categoriaActual } = useQuiosco();
+  console.log(categoriaActual);
 
   return (
     <Layout pagina={`Menú ${categoriaActual}`}>
@@ -17,6 +19,13 @@ export default function Home() {
       <p className="text-2xl my-10">
         Elige y personaliza tu pedido
       </p>
+      
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+        {categoriaActual?.productos?.map(producto => (
+          <Producto key={producto.id} producto={producto} />
+        ))}
+      </div>
+
     </Layout>
   );
 }
