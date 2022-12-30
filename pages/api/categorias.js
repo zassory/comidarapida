542ad7c676a83f7d589c61d71cf3cbf0b4db0d4c
zassory <nicolas.programador@gmail.com>
2022-12-30 19:04:38 -0300

    Eager loading en categorias

--- pages/api/categorias.js
+++ pages/api/categorias.js
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-
 export default async function handler(req, res) {
 
-  const categorias = await prisma.categoria.findMany();
-
+  const prisma = new PrismaClient();
+  const categorias = await prisma.categoria.findMany({
+    include: {
+      productos: true
+    }
+  });
   res.status(200).json(categorias);
 }
--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,11 @@ import { useQuiosco } from '../hooks/useQuiosco';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
- 
-  const { categoriaActual } = useQuiosco();
+   
+  const { categoriaActual } = useQuiosco();  
 
   return (
-    <Layout pagina={`Menú ${categoriaActual.nombre}`}>
+    <Layout pagina={`Menú ${categoriaActual}`}>
       <h1 className="text-4xl font-black">{categoriaActual?.nombre}</h1>
       <p className="text-2xl my-10">
         Elige y personaliza tu pedido
