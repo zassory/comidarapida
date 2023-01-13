f707bf42607f522db0ff4a4794a9e9d4252632fd
zassory <nicolas.programador@gmail.com>
2023-01-13 11:38:56 -0300

    select de ordenes listo

--- pages/api/ordenes.js
+++ pages/api/ordenes.js
@@ -2,9 +2,17 @@
 import { PrismaClient } from "@prisma/client";
 
 export default async function handler(req,res) {
-
     const prisma = new PrismaClient();
 
+    // Obtener Ordenes
+    const ordenes = await prisma.orden.findMany({
+        where: {
+            estado: false
+        }
+    });
+    res.status(200).json(ordenes);
+
+    // Crear Ordenes
     if(req.method === "POST"){
 
         const orden = await prisma.orden.create({
@@ -15,6 +23,6 @@ export default async function handler(req,res) {
                 fecha: req.body.fecha,
             },
         });        
-        res.json(orden);
+    res.status(200).json(orden);
     }
 }
