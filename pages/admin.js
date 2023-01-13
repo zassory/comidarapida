616149fea8b00a56df0efa74e9d6636b5447b319
zassory <nicolas.programador@gmail.com>
2023-01-13 12:45:18 -0300

    use de useSWR , mostrando datos y fetcher creado correctamente

--- pages/admin.js
+++ pages/admin.js
@@ -1,7 +1,16 @@
+import useSWR from 'swr';
+import axios from 'axios';
 import AdminLayout from "../layout/AdminLayout";
 
 export default function Admin() {
     
+    const fetcher = () => axios('/api/ordenes').then(datos => datos.data);
+    const { data, error, isLoading } = useSWR('/api/ordenes', fetcher);
+
+    console.log(data);
+    console.log(error);
+    console.log(isLoading);
+
     return (
         <AdminLayout pagina={'Admin'}>
             <h1 className="text-4xl font-black">Panel de Administración</h1>
