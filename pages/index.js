989ab42a7ce27f94cadb528c0f86a92e7770261b
zassory <nicolas.programador@gmail.com>
2023-01-14 19:43:59 -0300

    Actualizacion en tiempo real con 3 parametro en useSWR

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
   console.log(categoriaActual);
 
   return (
-    <Layout pagina={`Menú ${categoriaActual.nombre}`}>
+    <Layout pagina={`Menú ${categoriaActual?.nombre}`}>
       <h1 className="text-4xl font-black">{categoriaActual?.nombre}</h1>
       <p className="text-2xl my-10">
         Elige y personaliza tu pedido
