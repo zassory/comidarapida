6490133a1f8f104aa30b1bf0b463182fc6439123
zassory <nicolas.programador@gmail.com>
2023-01-14 01:24:47 -0300

    componente orden avanzado con estilos y mostrando imagen con Image de next

--- components/Orden.js
+++ components/Orden.js
@@ -1,7 +1,11 @@
+import Image from "next/image";
+import { formatearDinero } from "../helpers";
 
 export const Orden = ({orden}) => {
 
   const { id , nombre , total , pedido} = orden;
+
+  console.log(pedido);
 
   return (
     <div className="border p-10 space-y-5">
@@ -9,7 +13,28 @@ export const Orden = ({orden}) => {
         <p className="text-lg font-bold">Cliente: {nombre}</p>
 
         <div>
-            
+            {pedido.map(platillo => (
+                <div key={platillo.id} className="py-3 flex border-b last-of-type:border-0 items-center">
+                    <div className="w-32">
+                        <Image 
+                            width={400}
+                            height={500}
+                            src={`/assets/img/${platillo.imagen}.jpg`}
+                            alt={`Imagen Platillo ${platillo.nombre}`}
+                        />
+                    </div>
+
+                    <div className="p-5 space-y-2">
+                        <h4 className="text-xl font-bold text-amber-500">{platillo.nombre}</h4>
+                        <p className="text-lg font-bold">Cantidad: {platillo.cantidad}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+        <div className="md:flex md:items-center md:justify-between my-10">
+            <p className="mt-5 font-black text-4xl text-amber-500">
+                Total a pagar: {formatearDinero(total)}
+            </p>
         </div>
     </div>
   )
